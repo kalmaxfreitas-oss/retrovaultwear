8adbc4fc8c682c94ed20934365970b20cf223062
kalmaxfreitas-oss <kalmaxfreitas@gmail.com>
2026-03-17 10:03:20 -0300

    Update app.js

--- app.js
+++ app.js
@@ -62,6 +62,18 @@ const DATA = {
       ]
     },
      {
+      id: "One Piece",
+      name: "Coleção One Piece",
+      desc: "Camisetas inspiradas no universo Dragon Ball — retrô premium.",
+      cover: "images/colecoes/onepiece.jpg",
+      tags: ["anime", "retro", "street"],
+      products: [
+        { name: "Luffy", img: "images/produto/blanka.jpg", price: 45 , last: true , size: "M" },
+        { name: "Ice", img: "images/produto/blanka.jpg", price: 45 , last: true , size: "M" },
+        { name: "Zoro", img: "images/produto/blanka.jpg", price: 45 , last: true , size: "M" }
+      ]
+    },
+     {
       id: "Os Cavaleiros do Zodíaco",
       name: "Coleção Os Cavaleiros do Zodíaco",
       desc: "Clássicos do arcade em camisetas retrô — estilo limpo e premium.",
@@ -73,18 +85,6 @@ const DATA = {
         { name: "Guile",   img: "images/produto/blanka.jpg", price: 45 },
         { name: "Sagat",   img: "images/produto/blanka.jpg", price: 45 },
         { name: "Vega",    img: "images/produto/blanka.jpg", price: 45 }
-      ]
-    },
-     {
-      id: "One Piece",
-      name: "Coleção One Piece",
-      desc: "Camisetas inspiradas no universo Dragon Ball — retrô premium.",
-      cover: "images/colecoes/onepiece.jpg",
-      tags: ["anime", "retro", "street"],
-      products: [
-        { name: "Luffy", img: "images/produto/blanka.jpg", price: 45 , last: true , size: "M" },
-        { name: "Ice", img: "images/produto/blanka.jpg", price: 45 , last: true , size: "M" },
-        { name: "Zoro", img: "images/produto/blanka.jpg", price: 45 , last: true , size: "M" }
       ]
     },
   ]
